8cbdcdbc8c79856a53615ed967a202745c4b8311
ingyulee <ingyulee@tnear.com>
2022-09-27 16:05:30 +0900

    init with docker compose, docker file, spring boot demo

---
+++ application/src/main/java/com/peppermint100/application/Application.java
@@ -0,0 +1,23 @@
+package com.peppermint100.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@GetMapping("/hello")
+	public String helloController() {
+		return "Hello World " + ZonedDateTime.now();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
